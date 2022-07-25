c2bcbe67feb6a8b5e7c4480f28d38751a9f8e905
Vitor Henrique <vhenriquebm@gmail.com>
2022-07-25 20:08:46 -0300

    Criação de uma view costumizada para a tela de detalhes

---
+++ AppMusicBF/Home/CustomView/CustomNavBar.swift
@@ -0,0 +1,104 @@
+//
+//  CustomNavBar.swift
+//  AppMusicBF
+//
+//  Created by Vitor Henrique Barreiro Marinho on 25/07/22.
+//
+
+import UIKit
+
+class CustomNavBar: UIView {
+    
+    //MARK: - Properties
+    
+    lazy var categoryTitle: UILabel  = {
+        let label = UILabel ()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var cardTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var featureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var cardImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleToFill
+        image.clipsToBounds = true
+        return image
+    }()
+    
+    lazy var overLayView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black.withAlphaComponent(0.7)
+        return view
+    }()
+    
+    //MARK: - Overrides
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureViews()
+    }
+    
+    //MARK: - Public methods
+
+    public func configureView (data: CardViewModel){
+        cardTitle.text = data.cardTitle
+        categoryTitle.text = data.categoryName
+        cardImage.image = UIImage(named: data.cardImage ?? "")
+        featureLabel.attributedText = .featureText(data.likeCount ?? "", data.duration ?? "")
+    }
+    
+    //MARK: - Private methods
+    
+    private func configureViews () {
+        self.addSubview(categoryTitle)
+        self.addSubview(cardTitle)
+        self.addSubview(featureLabel)
+        self.addSubview(cardImage)
+        self.addSubview(overLayView)
+        self.configureConstraints()
+    }
+    
+    private func configureConstraints () {
+       
+        self.cardImage.pin(to: self)
+        self.overLayView.pin(to: self)
+        
+        NSLayoutConstraint.activate([
+        
+            self.categoryTitle.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 5),
+            self.categoryTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 20),
+            self.categoryTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            
+            self.cardTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 20),
+            self.cardTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -20),
+            self.cardTitle.topAnchor.constraint(equalTo: self.categoryTitle.bottomAnchor,constant: 5),
+
+            self.featureLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            self.featureLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            self.featureLabel.topAnchor.constraint(equalTo: self.cardTitle.bottomAnchor, constant: 5),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
